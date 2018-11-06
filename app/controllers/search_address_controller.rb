b0c6398a74b82d71feb0eb88c879170c498731ea
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-11-05 23:32:40 -0500

    raise exception on rest request

--- app/controllers/search_address_controller.rb
+++ app/controllers/search_address_controller.rb
@@ -8,10 +8,14 @@ class SearchAddressController < ApplicationController
 
     url = "https://www.googleapis.com/civicinfo/v2/voterinfo?address=#{address}&key=#{key}"
 
+    begin
     response = RestClient::Request.execute(
       method: :get, 
       url: "#{url}",
     )
+    rescue RestClient::ExceptionWithResponse => e
+    puts e.response
+    end
 
     render json: response 
 
