4fd5d4821640f5b5843fb4017c79e03239415123
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-11-06 04:57:59 -0500

    success message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import AddressForm from './AddressForm'
 
-const Home = (props) => (
-  <>
-  <h1 id="vote-h1">Vote 2018</h1>
-  <AddressForm/>
-  </>
-)
+const Home = (props) => {
+  if (props.voterInfo.length === 0) {
+    return <><h1 id="vote-h1">Vote 2018</h1><AddressForm/></>
+  }
+  else {
+    return <><h1 id="vote-h1">Vote 2018</h1><AddressForm/><br/><h3 class="success">Fetch Complete! Use Navigation to Browse Voter Info</h3></>
+  }
+ 
+}
   
 
 
