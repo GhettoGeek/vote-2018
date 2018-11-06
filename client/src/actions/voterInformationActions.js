e44163e37966b1f2f277efe43fe87c32bad4cac3
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-11-05 21:42:21 -0500

    changed action fetch url (removed local host)

--- client/src/actions/voterInformationActions.js
+++ client/src/actions/voterInformationActions.js
@@ -1,7 +1,7 @@
 export function fetchVoterInformation(address){
   return (dispatch) => {
     dispatch({ type: 'LOADING_VOTER_INFORMATION', address });
-    return fetch(`http://localhost:3001/search/${address}`)
+    return fetch(`/search/${address}`)
       .then(response => response.json())
       .then(data => dispatch({ type: 'FETCH_VOTER_INFORMATION', data }));
 }
