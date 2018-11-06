31c687343ec152c8075467d5d363c323d4c33b45
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-11-06 01:44:02 -0500

    added react-typekit

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Main from './components/Main'
 import { withRouter } from 'react-router-dom'
+import Typekit from 'react-typekit';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Typekit kitId="sff0zbx" />
         <nav className="nav"><NavBar></NavBar></nav>
          <Main voterInfo={this.props.voterInfo}/>
       </div>
